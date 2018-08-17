8baf0f0d1ec6ca345cd078d089d03af71eb00632
Eric Shull <eshulldesign@gmail.com>
2018-08-17 11:39:08 -0700

    add no functionality to hide and show div

--- src/app/booking-menu/booking-menu.component.ts
+++ src/app/booking-menu/booking-menu.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-booking-menu',
   templateUrl: './booking-menu.component.html',
   styleUrls: ['./booking-menu.component.css']
 })
-export class BookingMenuComponent implements OnInit {
+export class BookingMenuComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
