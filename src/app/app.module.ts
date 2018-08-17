3e7e7cc358157b6e7714c64aeed1db38e7befec2
Eric Shull <eshulldesign@gmail.com>
2018-08-17 10:18:12 -0700

    add book flight component and styling to center on page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { BookFlightComponent } from './book-flight/book-flight.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    BookFlightComponent
   ],
   imports: [
     BrowserModule,
